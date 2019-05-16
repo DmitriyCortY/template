0136a7a4db58af6bed993df364035b1bce83d986
DmitriyCortY <dmitriy.corty@gmail.com>
2019-05-16 20:18:17 +0300

    add structure

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -157,4 +157,23 @@ class ItemTracking{
     }
 }
 
+class Modal {
+    constructor(config) {
+        this.modalName = config.modalname
+
+        this.handler()
+    }
+    handler() {
+        $('[data-modal-trigger="' + this.modalName + '"]').on('click', e => {
+            this.toggleClass()
+        })
+    }
+    toggleClass() {
+        $('[data-modal-window="' + this.modalName + '"]').toggleClass('b-modal_open')
+    }
+}
+
+new Modal({
+    modalname: 'test'
+})
 //# sourceMappingURL=main.js.map
